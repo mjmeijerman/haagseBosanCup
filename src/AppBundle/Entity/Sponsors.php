540089abb507e749a3ac44b6aa0471a28c34d239
unknown <markmeijerman@hotmail.com>
2015-12-26 20:37:34 +0100

    sponsor entity

---
+++ src/AppBundle/Entity/Sponsors.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Table(name="sponsor")
+ */
+class Sponsor
+{
+    private $temp;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(length=300)
+     */
+    protected $locatie;
+
+    /**
+     * @Assert\File(
+     *      maxSize="5M",
+     *      mimeTypes = {"image/gif", "image/jpeg", "image/pjpeg", "image/png"},
+     *      mimeTypesMessage = "Please upload a valid image: gif, jpg or png"
+     *      )
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(length=300, nullable=true)
+     */
+    protected $naam;
+
+    /**
+     * @ORM\Column(length=300, nullable=true)
+     */
+    protected $website;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $omschrijving;
+
+
+    public function getAll()
+    {
+        $items = new \stdClass();
+        $items->id = $this->id;
+        $items->locatie = $this->locatie;
+        return $items;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->locatie
+            ? null
+            : $this->getUploadRootDir().'/'.$this->locatie;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->locatie
+            ? null
+            : $this->getUploadDir().'/'.$this->locatie;
+    }
+
+    public function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/sponsors';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set locatie
+     *
+     * @param string $locatie
+     * @return Sponsor
+     */
+    public function setLocatie($locatie)
+    {
+        $this->locatie = $locatie;
+
+        return $this;
+    }
+
+    /**
+     * Get locatie
+     *
+     * @return string
+     */
+    public function getLocatie()
+    {
+        return $this->locatie;
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        if (isset($this->locatie)) {
+            $this->temp = $this->locatie;
+            $this->locatie = null;
+        } else {
+            $this->locatie = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->locatie = $filename.'.'.$this->getFile()->getClientOriginalExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+        $this->getFile()->move($this->getUploadRootDir(), $this->locatie);
+        if (isset($this->temp)) {
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function storeFilenameForRemove()
+    {
+        $this->temp = $this->getAbsolutePath();
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if (isset($this->temp)) {
+            unlink($this->temp);
+        }
+    }
+}
