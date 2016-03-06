cae5e362aff4ae6f146a52b2b1652f9a45ef3be8
unknown <markmeijerman@hotmail.com>
2016-03-06 18:16:10 +0100

    jurylid toevoegen inloggedeelte

--- src/AppBundle/Controller/ContactpersoonController.php
+++ src/AppBundle/Controller/ContactpersoonController.php
@@ -26,6 +26,7 @@ use AppBundle\Controller\BaseController;
 use AppBundle\Form\Type\ContentType;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
 
 /**
  * @Security("has_role('ROLE_CONTACT')")
@@ -234,6 +235,156 @@ class ContactpersoonController extends BaseController
             'vrijePlekken' => $vrijePlekken,
             'turnster' => $turnster,
             'geboorteJaren' => $geboorteJaren,
+            'classNames' => $classNames,
+            'csrfToken' => $csrfToken,
+        ));
+    }
+
+    /**
+     * @Route("/contactpersoon/addJury/", name="addJury")
+     * @Method({"GET", "POST"})
+     */
+    public function addJury(Request $request)
+    {
+        $this->setBasicPageData();
+        $jury = [
+            'voornaam' => '',
+            'achternaam' => '',
+            'email' => '',
+            'brevet' => '',
+            'opmerking' => '',
+            'dag' => '',
+        ];
+        $classNames = [
+            'voornaam' => 'text',
+            'achternaam' => 'text',
+            'email' => 'text',
+            'brevet' => 'turnster_niveau',
+            'opmerking' => 'text',
+            'dag' => 'turnster_niveau',
+        ];
+        $csrfToken = $this->getToken();
+        if ($request->getMethod() == 'POST') {
+            $jury = [
+                'voornaam' => $request->request->get('voornaam'),
+                'achternaam' => $request->request->get('achternaam'),
+                'email' => $request->request->get('email'),
+                'brevet' => $request->request->get('brevet'),
+                'dag' => $request->request->get('dag'),
+                'opmerking' => $request->request->get('opmerking'),
+            ];
+            $postedToken = $request->request->get('csrfToken');
+            if (!empty($postedToken)) {
+                if ($this->isTokenValid($postedToken)) {
+                    $validationJury = [
+                        'voornaam' => false,
+                        'achternaam' => false,
+                        'email' => false,
+                        'brevet' => false,
+                        'dag' => false,
+                        'opmerking' => true,
+                    ];
+
+                    $classNames['opmerking'] = 'succesIngevuld';
+
+                    if (strlen($request->request->get('voornaam')) > 1) {
+                        $validationJury['voornaam'] = true;
+                        $classNames['voornaam'] = 'succesIngevuld';
+                    } else {
+                        $this->addFlash(
+                            'error',
+                            'geen geldige voornaam ingevoerd'
+                        );
+                        $classNames['voornaam'] = 'error';
+                    }
+
+                    if (strlen($request->request->get('achternaam')) > 1) {
+                        $validationJury['achternaam'] = true;
+                        $classNames['achternaam'] = 'succesIngevuld';
+                    } else {
+                        $this->addFlash(
+                            'error',
+                            'geen geldige achternaam ingevoerd'
+                        );
+                        $classNames['achternaam'] = 'error';
+                    }
+
+                    if (strlen($request->request->get('email')) > 1) {
+                        $emailConstraint = new EmailConstraint();
+                        $errors = $this->get('validator')->validate(
+                            $request->request->get('email'),
+                            $emailConstraint
+                        );
+                        if (count($errors) == 0) {
+                            $validationJury['email'] = true;
+                            $classNames['email'] = 'succesIngevuld';
+                        } else {
+                            foreach ($errors as $error) {
+                                $this->addFlash(
+                                    'error',
+                                    $error->getMessage()
+                                );
+                            }
+                            $classNames['email'] = 'error';
+                        }
+                    } else {
+                        $this->addFlash(
+                            'error',
+                            'geen email ingevoerd'
+                        );
+                        $classNames['email'] = 'error';
+                    }
+
+                    if ($request->request->get('brevet')) {
+                        $validationJury['brevet'] = true;
+                        $classNames['brevet'] = 'brevet';
+                    } else {
+                        $this->addFlash(
+                            'error',
+                            'geen brevet ingevoerd'
+                        );
+                        $classNames['brevet'] = 'error';
+                    }
+
+                    if ($request->request->get('dag')) {
+                        $validationJury['dag'] = true;
+                        $classNames['dag'] = 'succesIngevuld';
+                    } else {
+                        $this->addFlash(
+                            'error',
+                            'geen dag ingevoerd'
+                        );
+                        $classNames['dag'] = 'error';
+                    }
+                    if (!(in_array(false, $validationJury))) {
+                        $jurylid = new Jurylid();
+                        $jurylid->setVoornaam($request->request->get('voornaam'));
+                        $jurylid->setAchternaam($request->request->get('achternaam'));
+                        $jurylid->setEmail($request->request->get('email'));
+                        $jurylid->setBrevet($request->request->get('brevet'));
+                        $jurylid->setOpmerking($request->request->get('opmerking'));
+                        if ($request->request->get('dag') == 'za') {
+                            $jurylid->setZaterdag(true);
+                            $jurylid->setZondag(false);
+                        } elseif ($request->request->get('dag') == 'zo') {
+                            $jurylid->setZaterdag(false);
+                            $jurylid->setZondag(true);
+                        } else {
+                            $jurylid->setZaterdag(true);
+                            $jurylid->setZondag(true);
+                        }
+                        $jurylid->setUser($this->getUser());
+                        $this->getUser()->addJurylid($jurylid);
+                        $this->addToDB($this->getUser());
+                        return $this->redirectToRoute('getContactpersoonIndexPage');
+                    }
+                }
+            }
+        }
+        return $this->render('contactpersoon/addJury.html.twig', array(
+            'menuItems' => $this->menuItems,
+            'sponsors' => $this->sponsors,
+            'jury' => $jury,
             'classNames' => $classNames,
             'csrfToken' => $csrfToken,
         ));
