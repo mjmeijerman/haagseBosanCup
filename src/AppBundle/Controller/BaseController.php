fe4a0c19dcdf9c592872a8b47bfaf74e76cf6a30
Mark Meijerman <mark@future500.nl>
2019-05-15 15:49:52 +0200

    update niveaus

--- src/AppBundle/Controller/BaseController.php
+++ src/AppBundle/Controller/BaseController.php
@@ -317,7 +317,7 @@ class BaseController extends Controller
     {
         switch ($categorie) {
             case 'Voorinstap': return date('Y', time()) - 8;
-            case '(Voor)instap': return [date('Y', time()) - 9, date('Y', time()) - 9];
+            case '(Voor)instap': return [date('Y', time()) - 9, date('Y', time()) - 8];
             case 'Instap': return date('Y', time()) - 9;
             case 'Pupil 1': return date('Y', time()) - 10;
             case 'Pupil 2': return date('Y', time()) - 11;
