443f2e8b15cd41bcb6fa120341905d141a07dbc0
Mark Meijerman <mark@future500.nl>
2019-05-20 17:25:25 +0200

    do not create new turnster when vloermuziek already exists

--- src/AppBundle/Entity/Vloermuziek.php
+++ src/AppBundle/Entity/Vloermuziek.php
@@ -37,7 +37,7 @@ class Vloermuziek
     private $file;
 
     /**
-     * @ORM\OneToOne(targetEntity="Turnster", mappedBy="vloermuziek", cascade={"persist", "remove"})
+     * @ORM\OneToOne(targetEntity="Turnster", mappedBy="vloermuziek", cascade={"persist"})
      * @var Turnster
      */
     private $turnster;
